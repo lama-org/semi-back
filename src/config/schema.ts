c11f3a1f775d36a2813b17b167c64359cc79b50f
AndrewLEEJM <woals214@gmail.com>
2021-08-22 19:14:19 +0900

    feat/graphql 개발 셋팅
    우선순위 1 - graphql 개발 셋팅 #7에 대한 작업 완료

---
+++ src/config/schema.ts
@@ -0,0 +1,23 @@
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { mergeTypeDefs, mergeResolvers } from "@graphql-tools/merge";
+import { loadFilesSync } from "@graphql-tools/load-files";
+import path from 'path';
+
+// api폴더 내부의 graphql 확장자를 가진 모든 파일을 배열 형태로 가져옴
+const allTypes = loadFilesSync(path.join(__dirname, '../api/**/*.graphql'));
+
+// api폴더 내부의 resolvers.* 확장자를 가진 모든 파일을 배열 형태로 가져옴
+const allResolvers = loadFilesSync(
+  path.join(__dirname, '../api/**/*.ts')
+);
+ 
+
+// 스키마 생성
+const schema = makeExecutableSchema({
+  // 가져온 graphql 타입들을 하나로 합쳐줌
+  typeDefs: mergeTypeDefs(allTypes),
+  // 가져온 resover 함수들을 하나로 합쳐줌
+  resolvers: mergeResolvers(allResolvers),
+});
+ 
+export default schema;
