533e5cb7869ade21a68268eb1d25a8550c874514
develjsh <develJsh@gmail.com>
2021-08-25 08:26:32 +0900

    feat: Movie Api 생성

--- src/movie/movie.resolvers.ts
+++ src/movie/movie.resolvers.ts
@@ -1,7 +1,29 @@
+import { IObjectTypeResolver } from '@graphql-tools/utils/Interfaces';
+import { getMovieDetail, getNowPlaying, getPopular, getTopRated, getUpcoming } from '../api/movie/movie-api';
+
 const resolvers = {
   Query: {
-    movies: () => [{ title: '코딩 사나이', releaseDate: '2021-08-22' }],
-  },
+    movie_detail: async (_, { id }: { id: number }) => {
+      const { data } = await getMovieDetail(id);
+      return data;
+    },
+    now_playing: async (_, { page }: { page: number }, { region }: { region: string }) => {
+      const { data } = await getNowPlaying(page, region);
+      return data;
+    },
+    moviePopular: async (_, { page }: { page: number }, { region }: { region: string }) => {
+      const { data } = await getPopular(page, region);
+      return data;
+    },
+    top_rated: async (_, { page }: { page: number }, { region }: { region: string }) => {
+      const { data } = await getTopRated(page, region);
+      return data;
+    },
+    upcoming: async (_, { page }: { page: number }, { region }: { region: string }) => {
+      const { data } = await getUpcoming(page, region);
+      return data;
+    },
+  } as IObjectTypeResolver,
 };
 
 export default resolvers;
