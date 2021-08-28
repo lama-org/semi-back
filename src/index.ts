739850823bb8501af18838176aa4da08568e430c
wooseok <dntjr031@gmail.com>
2021-08-28 11:44:26 +0900

    fix: fixed cors error

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { typeDefs, resolvers } from './schema';
 import MovieAPI from './api/movie';
 import TvAPI from './api/tv';
 import express from 'express';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 4000;
 
@@ -22,17 +23,7 @@ async function startServer() {
   await server.start();
 
   const app = express();
-
-  app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json',
-    );
-    next();
-  });
+  app.use(cors());
 
   server.applyMiddleware({ app });
 
