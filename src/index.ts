cc906d08afa7f2af4df1cf30e8e4491e04fb63a9
wooseok <dntjr031@gmail.com>
2021-08-28 11:33:06 +0900

    fix: fixed cors error

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,44 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
 import { typeDefs, resolvers } from './schema';
 import MovieAPI from './api/movie';
 import TvAPI from './api/tv';
+import express from 'express';
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  dataSources: () => {
-    return {
-      movieAPI: new MovieAPI(),
-      tvAPI: new TvAPI(),
-    };
-  },
-});
+const PORT = process.env.PORT || 4000;
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+async function startServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+      return {
+        movieAPI: new MovieAPI(),
+        tvAPI: new TvAPI(),
+      };
+    },
+  });
+  await server.start();
+
+  const app = express();
+
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json',
+    );
+    next();
+  });
+
+  server.applyMiddleware({ app });
+
+  await new Promise((r: any) => app.listen({ port: PORT }, r));
+
+  console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+}
+
+startServer().then();
